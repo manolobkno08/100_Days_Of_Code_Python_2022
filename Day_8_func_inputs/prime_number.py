4c2a49e95e251c0b08aee15e365f8317b3559375
manolobkno08 <manologm10@hotmail.com>
2022-03-30 23:28:39 -0500

    ADD: Day 8 completed

--- Day_8_func_inputs/prime_number.py
+++ Day_8_func_inputs/prime_number.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 
 def prime_checker(number):
-    if number > 1:
-        for i in range(2, number):
-            if (number % i) == 0:
-                print("It's not a prime number.")
-                break
-        else:
-            print("It's a prime number.")
-
+    is_prime = True
+    for i in range(2, number):
+        if number % i == 0:
+            is_prime = False
+    if is_prime:
+        print("It's a prime number")
     else:
-        print("It's not a prime number.")
+        print("It's not a prime number")
 
 
 if __name__ == '__main__':
